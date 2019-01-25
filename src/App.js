2521920e0b4806f03be07c49cd7d3a9a487aea05
Mstapleton22 <mike.stapleton2@gmail.com>
2019-01-25 14:35:28 -0700

    creating front end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import Nav from './components/Nav.js'
 import Card from './components/Card';
+import Inputbox from './components/Inputbox';
+import CardData from './CardData.json'
+import Button from './components/Button.js';
 import './App.css';
 
 class App extends Component {
@@ -7,34 +11,102 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: []
+      data: CardData.methods,
+      cardExist: false,
+      currentCard: '',
+      input: '',
+      inputValue: false,
+      id: 0,
+
     }
   }
+
+  getRandomCard(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+  generateRandom = () => {
+    const randomIndex = this.getRandomCard(this.state.data.length - 1)
+    const randomCard = this.state.data[randomIndex]
+    this.setState({
+      cardExist: true,
+      currentCard: randomCard,
+    })
+  }
+
+  generateNext = (event) => {
+    event.preventDefault()
+    const id = this.state.id
+    if (id >= this.state.data.length - 1) {
+      this.setState({
+        currentCard: this.state.data[id],
+        id: 0
+      })
+    } else {
+      this.setState({
+        cardExist: true,
+        currentCard: this.state.data[id],
+        id: this.state.id + 1
+      })
+    }
+  }
+
+  inputAnswer = () => {
+    console.log('called')
+    if (this.state.currentCard.name === this.state.input) {
+      this.setState({
+        inputValue: true
+      })
+    }
+  }
+  updateInput = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  // generatePrev = (event) => {
+  //   event.preventDefault()
+  //   const id = this.state.id
+  //   if (id <= this.state.data.length - 1) {
+  //     this.setState({
+  //       currentCard: this.state.data[id],
+  //       id: this.state.id
+  //     })
+  //   } else {
+  //     this.setState({
+  //       cardExist: true,
+  //       currentCard: this.state.data[id] - 1,
+  //       id: this.state.id - 1
+  //     })
+  //   }
+  // }
+
+
   render() {
+    console.log('cardexists')
     return (
       <div className="container-fluid">
-        <div className="row navbar">
-          <h3>JavaScript Flashcards</h3>
-          <ul className="nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="@" target="blank">Hard Data</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="mailto:mike.stapleton2@gmail.com">Contact me</a>
-            </li>
-          </ul>
-        </div>
-        {<Card />}
-        <div className="row justify-content-center mt-5">
-          <div className="btn-group" role="group" aria-label="Basic example">
-            <button type="button" className="btn btn-success">Easy</button>
-            <button type="button" className="btn btn-warning">Medium</button>
-            <button type="button" className="btn btn-danger">Hard</button>
+        <Nav />
+        <div className="body">
+          {this.state.cardExist
+            ? <Card
+              currentCard={this.state.currentCard}
+            />
+            : ''}
+          {/* <h6 className="row header justify-content-center mt-5">Click "Easy" "Medium" "Hard" to display a new card in that category</h6> */}
+          {<Button
+            generateRandom={this.generateRandom}
+            // generatePrev={this.generatePrev}
+            generateNext={this.generateNext}
+          />}
+          {<Inputbox
+            updateInput={this.updateInput}
+            inputAnswer={this.inputAnswer}
+          />}
+          <div className="row justify-content-center mb-5 mt-5">
+            <button type="button" class="btn btn-success mb-5 mt-2 mr-5">Correct! Now remove</button>
+            <button type="button" class="btn btn-danger mb-5 mt-2 ml-5">Wrong!Keep in deck</button>
           </div>
-        </div>
-        <div className="row">
-          <button type="button" class="btn btn-success">Correct!</button>
-          <button type="button" class="btn btn-danger">Keep in deck</button>
         </div>
       </div >
     );
