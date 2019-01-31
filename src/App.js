92c6d44e496254a548645b36496f92889801c352
Mstapleton22 <mike.stapleton2@gmail.com>
2019-01-31 15:57:32 -0700

    created routes to navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { Router, Route } from "react-router-dom";
+// import AppRouter from "./components/AppRouter.js";
 import Nav from './components/Nav.js'
-import Card from './components/Card';
-import Inputbox from './components/Inputbox';
-// import CardData from './CardData.json'
-import Button from './components/Button.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './components/Home.js';
 import AddCard from './components/AddCard.js';
+import Body from './components/Body.js';
 import './App.css';
 
 class App extends Component {
@@ -19,7 +18,6 @@ class App extends Component {
       input: '',
       inputValue: false,
       id: 0,
-
     }
   }
 
@@ -36,6 +34,7 @@ class App extends Component {
   getRandomCard(max) {
     return Math.floor(Math.random() * (max + 1));
   }
+
   generateRandom = () => {
     const randomIndex = this.getRandomCard(this.state.data.length - 1)
     const randomCard = this.state.data[randomIndex]
@@ -54,13 +53,14 @@ class App extends Component {
         cardExist: true,
         currentCard: this.state.data[id],
         id: this.state.id + 1,
-        inputValue: false
+        inputValue: false,
+        input: '',
       })
     } else if (id === this.state.data.length) {
       console.log('else')
       this.setState({
         currentCard: this.state.data[0],
-        id: 0 + 1,
+        id: 1,
         inputValue: false
       })
     }
@@ -69,6 +69,8 @@ class App extends Component {
   inputAnswer = () => {
     return this.state.currentCard.name === this.state.input ? this.setState({ inputValue: true }) : ''
   }
+  // ON TRUE event.target.value = '' in a few if statements
+  // ON FALSE event.target.value = this.state.input
 
   updateInput = (event) => {
     this.setState({
@@ -76,59 +78,30 @@ class App extends Component {
     })
   }
 
-  // generatePrev = (event) => {
-  //   event.preventDefault()
-  //   const id = this.state.id
-  //   if (id <= this.state.data.length - 1) {
-  //     this.setState({
-  //       currentCard: this.state.data[id],
-  //       id: this.state.id
-  //     })
-  //   } else {
-  //     this.setState({
-  //       cardExist: true,
-  //       currentCard: this.state.data[id] - 1,
-  //       id: this.state.id - 1
-  //     })
-  //   }
-  // }
 
 
   render() {
-    // console.log(this.state.data)
-    return (
-      <div className="container-fluid">
-        {/* <Router>
-          <div>
-            <Route exact path="/" component={App.js} />
-            // components that are dynamically rendered on the click.
-            <Route path="/AddCard" component={AddCard} />
-          </div>
-        </Router> */}
+    console.log("looping")
 
-        <Nav />
-        <div className="body">
-          {this.state.cardExist
-            ? <Card
-              currentCard={this.state.currentCard}
-            />
-            : ''}
-          {<Button
+    return (
+      <Router>
+        <div className="container-fluid">
+
+          <Nav
+          />
+          <Route path="/home" render={() => <Home />} />
+          <Route path="/addcard" render={() => <AddCard />} />
+          <Route path="/study" render={() => <Body
+            currentCard={this.state.currentCard}
+            cardExist={this.state.cardExist}
             generateRandom={this.generateRandom}
-            // generatePrev={this.generatePrev}
             generateNext={this.generateNext}
-          />}
-          {<Inputbox
             inputValue={this.state.inputValue}
             updateInput={this.updateInput}
             inputAnswer={this.inputAnswer}
-          />}
-          {/* <div className="row justify-content-center mb-5 mt-5">
-            <button type="button" class="btn btn-success mb-5 mt-2 mr-5">Correct! Now remove</button>
-            <button type="button" class="btn btn-danger mb-5 mt-2 ml-5">Wrong!Keep in deck</button>
-          </div> */}
+          />} />
         </div>
-      </div >
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import App from './App';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />,
-  // <Router>
-  //   <App />
-  // </Router>,
+ReactDOM.render(
+  <App />,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
